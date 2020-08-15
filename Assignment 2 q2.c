51e1f785df20b29ab5363d8f374f0335de0c99b5
Varun <42139581+varunmanilal@users.noreply.github.com>
2020-08-15 16:39:49 +0530

    Update Assignment 2 q2.c

--- Assignment 2 q2.c
+++ Assignment 2 q2.c
@@ -4,16 +4,16 @@ int main(){
 	double vectorA[3]={2.3, 5, 6};
 	double vectorB[3]={6.1, 5.7, 8};
 	double C[3] = {0, 0, 0};
-	//A+B
-	for(int i=0;i<3;i++){
-		C[i]=vectorA[i]+vectorB[i];
-		printf("%f",C[i]);
-		printf(" ");
-	}
+	double D=0;
 	//A*B
-	printf("\n");
+	for(int i=0;i<3;i++){
+		D+=vectorA[i]*vectorB[i];
+	}
+	printf("The dot product A.B is: %f",D);
+	//A+B
+	printf("\nThe sum A+B is: \n");
 		for(int j=0;j<3;j++){
-		C[j]=vectorA[j]*vectorB[j];
+		C[j]=vectorA[j]+vectorB[j];
 		printf("%f",C[j]);
 		printf(" ");
 	}
