bdd155656bec6a80794d34c2dbd51b7b4a4e512e
Varun <42139581+varunmanilal@users.noreply.github.com>
2020-08-18 12:26:06 +0530

    Update Assignment 2 q2.c

--- Assignment 2 q2.c
+++ Assignment 2 q2.c
@@ -6,11 +6,13 @@ int main(){
 	double C[3] = {0, 0, 0};
 	double D=0;
 	//A*B
+	//The dot product A.B is: 90.530000
 	for(int i=0;i<3;i++){
 		D+=vectorA[i]*vectorB[i];
 	}
 	printf("The dot product A.B is: %f",D);
 	//A+B
+	//The sum A+B is:8.400000 10.700000 14.000000 
 	printf("\nThe sum A+B is: \n");
 		for(int j=0;j<3;j++){
 		C[j]=vectorA[j]+vectorB[j];
